4b5934580268a4a34bb43c2fd619df1857a599e9
Fabio <fabio.delbo@gmail.com>
2024-08-05 23:12:29 +0100

    requirements

--- my_classes.py
+++ my_classes.py
@@ -34,6 +34,8 @@ from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import RandomForestClassifier
 
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.regularizers import l1, l2
 from tensorflow.keras.layers import Dropout
 from tensorflow import keras
 from tensorflow.keras.preprocessing.sequence import TimeseriesGenerator
